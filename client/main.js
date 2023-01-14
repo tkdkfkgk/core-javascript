ed9cf7d92c01f81731e5b56944269a67b3ebc5f7
tkdkfkgk <mun961@gmail.com>
2023-01-14 15:48:18 +0900

    [추가] 주접 생성기

--- client/main.js
+++ client/main.js
@@ -1,1 +1,39 @@
-console.log(window);
+import { clearContents, getInputValue, getNode, getRandom, insertLast, isNumericString, showAlert,  } from './lib/index.js';
+
+import { jujeobData } from "./data/data.js";
+
+// console.log(1, isNaN == 12);
+// console.log(isNumericString(123));
+
+const submit = getNode('#submit');
+const resultArea = getNode('.result');
+
+console.log(submit);
+
+
+function clickSubmitHandler(e){
+  e.preventDefault(); // 브라우저 기능 새로고침 차단
+
+  let name = getInputValue('#nameField'); // input의 값 가져오기
+  let list = jujeobData(name);
+  let pick = list[getRandom(list.length-1)]
+
+  // console.log(list);
+  // console.log(pick);
+showAlert
+  // let result = pick;
+  if(!name){
+    showAlert('.alert-error','이름을 입력해주세요 !',3000)
+    return;
+  }
+
+  if(isNumericString(name)){
+    showAlert('.alert','제대로된 이름을 입력해주세요 !',3000)
+  }
+
+  clearContents(resultArea)
+  insertLast(resultArea,pick)
+}
+
+submit.addEventListener('click',clickSubmitHandler)
+
