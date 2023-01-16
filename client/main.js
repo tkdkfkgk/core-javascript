7b3a0c90054e59abae9e72f306ae594a68298224
tkdkfkgk <mun961@gmail.com>
2023-01-16 11:32:08 +0900

    [추가] 배열의 구조 분해 할당

--- client/main.js
+++ client/main.js
@@ -1,4 +1,7 @@
-import { diceAnimation, getNode } from "./lib/index.js";
+import { 
+  diceAnimation, 
+  getNode, 
+  getNodes } from "./lib/index.js";
 
 // [ 주사위 굴리기 ]
 // 1. dice 애니메이션 불러오기
@@ -15,8 +18,14 @@ import { diceAnimation, getNode } from "./lib/index.js";
 
 // diceAnimation()
 
-// 1. 주사위굴리기 버튼 접근
-const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+// 1. 주사위굴리기 버튼 접근 > 배열의 구조 분해 할당
+const [rollingDiceButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
+
+// console.log(button);
+// const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+// const recordButton = getNode('.buttonGroup > button:nth-child(2)');
+// const resetButton = getNode('.buttonGroup > button:nth-child(3)');
+
 
 // IIFE
 
@@ -30,9 +39,11 @@ const handlerRollingDice = (() => {
     if(!isRolling){
       console.log(1);
       stopAnimation = setInterval(diceAnimation,1000)
+      recordButton.disabled = true;
     }else{
       console.log(2);
       clearInterval(stopAnimation);
+      recordButton.disabled = false;
     }
   
     isRolling = !isRolling; // 2번 클릭시마다 t/f 값을 바꿔줘서 if의 구문을 토글기능처럼 사용
