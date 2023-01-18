22657d7497f771f8bdf9d571a2496f7e28c26de8
tkdkfkgk <mun961@gmail.com>
2023-01-18 11:12:08 +0900

    [추가] delayP 함수 만들기

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -1,59 +1,104 @@
-import { getNode  } from "../dom/getNode.js";
 
-const first = getNode('.first')
+import { getNode } from '../dom/getNode.js'
+import { isNumber,isObject } from './typeOf.js'
+
+
+
+const first = getNode('.first');
+const second = getNode('.second');
+
+
 
 function delay(callback,timeout = 1000){
-  setTimeout(callback,timeout);
+  setTimeout(callback, timeout);
 }
 
-/* delay(()=>{
+
+// first.style.top = '-100px';
+// first.style.transform = 'rotate(360deg)';
+// first.style.top = '0px';
+
+/* 
+delay(()=>{
   first.style.top = '-100px';
   delay(()=>{
-    first.style.transform = 'rotate(360deg)';
+    second.style.left = '100px';
     delay(()=>{
       first.style.top = '0px';
+      second.style.left = '0px';
     })
+    first.style.transform = 'rotate(360deg)';
   })
 })
+ */
 
-first.style.transform = 'rotate(360deg)';
-first.style.top = '0px'; */
-
-
-/* delayP()
+/* 
+delayP()
 .then(()=>{
   first.style.top = '-100px';
   return delayP()
 })
 .then(()=>{
   first.style.transform = 'rotate(360deg)';
+  second.style.left = '100px';
   return delayP()
 })
 .then(()=>{
   first.style.top = '0px';
-}) */
+  second.style.left = '0px';
+})
+ */
 
-function delayP(shouldReject = false, timeout = 1000, data = '성공했습니다.', errorMessage = '알 수 없는 오류가 발생했습니다.'){
+const defaultOptions = {
+  shouldReject: false,
+  timeout: 1000,
+  data:'성공',
+  errorMessage: '알 수 없는 오류가 발생했습니다.'
+}
 
-  return new Promise((resolve, reject)=>{
+export function delayP(options = {}){
+
+  // defaultOptions
+
+  let config = {...defaultOptions}
+     
+  
+  if(isNumber(options)){
+    config.timeout = options;
+  }
+
+  // 객체 합성  mixin
+  if(isObject(options)){
+    config = {...config,...options};
+  }
+  
+  
+  
+  const {shouldReject,data,errorMessage,timeout} = config;
+  
+  
+  return new Promise((resolve, reject) => {
+    
     setTimeout(() => {
       !shouldReject ? resolve(data) : reject(errorMessage);
-      
     }, timeout);
-    
   })
 }
 
-delayP(false,1000,'진짜 성공','오류가 발생했다!!')
-.then((res)=>{
-  console.log(res);
-})
+
+// delayP(3000).then((res)=>{
+//   console.log(res); // 진짜 성공
+// })
+
 
 // delayP()
 // .then(res=>console.log(res))
 // .catch(err=>console.log(err))
 
-// console.log(delayP());
 
 
-// console.log(first);
+
+
+
+
+// async await
