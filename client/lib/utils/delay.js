510ae08b660398e9f02947734092f3695ba09f8d
tkdkfkgk <mun961@gmail.com>
2023-01-18 10:29:09 +0900

    [추가] delayP parameter 설정하기

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -6,7 +6,7 @@ function delay(callback,timeout = 1000){
   setTimeout(callback,timeout);
 }
 
-delay(()=>{
+/* delay(()=>{
   first.style.top = '-100px';
   delay(()=>{
     first.style.transform = 'rotate(360deg)';
@@ -16,9 +16,44 @@ delay(()=>{
   })
 })
 
-// first.style.transform = 'rotate(360deg)';
-// first.style.top = '0px';
+first.style.transform = 'rotate(360deg)';
+first.style.top = '0px'; */
 
+
+/* delayP()
+.then(()=>{
+  first.style.top = '-100px';
+  return delayP()
+})
+.then(()=>{
+  first.style.transform = 'rotate(360deg)';
+  return delayP()
+})
+.then(()=>{
+  first.style.top = '0px';
+}) */
+
+function delayP(shouldReject = false, timeout = 1000, data = '성공했습니다.', errorMessage = '알 수 없는 오류가 발생했습니다.'){
+
+  return new Promise((resolve, reject)=>{
+    setTimeout(() => {
+      !shouldReject ? resolve(data) : reject(errorMessage);
+      
+    }, timeout);
+    
+  })
+}
+
+delayP(false,1000,'진짜 성공','오류가 발생했다!!')
+.then((res)=>{
+  console.log(res);
+})
+
+// delayP()
+// .then(res=>console.log(res))
+// .catch(err=>console.log(err))
+
+// console.log(delayP());
 
 
 // console.log(first);
