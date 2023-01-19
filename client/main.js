fdd99e29aa6e92d08d40e23252475608bc9f0a77
tkdkfkgk <mun961@gmail.com>
2023-01-19 16:43:05 +0900

    [추가] storage utils함수 만들기

--- client/main.js
+++ client/main.js
@@ -1,2 +1,14 @@
-import {} from "./lib/index.js"
+import { getNode, loadStorage, saveStorage } from "./lib/index.js"
 
+const textField = getNode('#textField');
+const deleteButton = getNode('input[value="삭제"]');
+
+loadStorage('area').then((res)=>{
+  textField.value = res;
+})
+
+function inputHandler(){
+  saveStorage('area',textField.value);
+}
+
+textField.addEventListener('input',inputHandler)
